8793c2fa966f782ccc4506e17d470226171b26f2
SchiauAlessio <alessio.schiau1@gmail.com>
2022-05-07 16:21:17 +0300

    errors

--- DiscordBotHikari/Data/build.sql
+++ DiscordBotHikari/Data/build.sql
@@ -1,4 +1,11 @@
 CREATE TABLE IF NOT EXISTS guilds(
     GuildID integer primary key,
     Prefix TEXT DEFAULT "$$"
-)
+);
+
+CREATE TABLE IF NOT EXISTS errors (
+    err_id TEXT PRIMARY KEY,
+    err_time NUMERIC DEFAULT CURRENT_TIMESTAMP,
+    err_cmd TEXT,
+    err_text TEXT
+);
